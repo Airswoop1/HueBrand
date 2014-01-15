27477c0e839ef59e0e6616d4dff21657d0c8fe18
Eugene Joseph <e@ej.vc>
2014-01-14 22:41:53 -0500

    design changes

--- application/public/js/main.js
+++ application/public/js/main.js
@@ -336,76 +336,8 @@ $(document).ready(function(){
 
 // #Module - Regional Attribute Associations
 
-    // Google Maps
-    function google_maps() {
-        new GMaps({
-            div: '#map',
-            lat: -12.043333,
-            lng: -77.028333
-        });
-
-        url = GMaps.staticMapURL({
-            size: [610, 300],
-            lat: -12.043333,
-            lng: -77.028333
-        });
-
-        $('<img/>').attr('src', url)
-            .appendTo('#static');
-
-        map = new GMaps({
-            div: '#route',
-            lat: -12.043333,
-            lng: -77.028333
-        });
-        $('#start_travel').click(function (e) {
-            e.preventDefault();
-            map.travelRoute({
-                origin: [-12.044012922866312, -77.02470665341184],
-                destination: [-12.090814532191756, -77.02271108990476],
-                travelMode: 'driving',
-                step: function (e) {
-                    $('#instructions').append('<li>' + e.instructions + '</li>');
-                    $('#instructions li:eq(' + e.step_number + ')').delay(450 * e.step_number).fadeIn(200, function () {
-                        map.setCenter(e.end_location.lat(), e.end_location.lng());
-                        map.drawPolyline({
-                            path: e.path,
-                            strokeColor: '#131540',
-                            strokeOpacity: 0.6,
-                            strokeWeight: 6
-                        });
-                    });
-                }
-            });
-        });
-
-        var addresspicker = $("#addresspicker").addresspicker();
-        var addresspickerMap = $("#addresspicker_map").addresspicker({
-            regionBias: "de",
-            map: "#map_canvas",
-            typeaheaddelay: 1000,
-            mapOptions: {
-                zoom: 16,
-                center: new google.maps.LatLng(52.5122, 13.4194)
-            }
-
-        });
-
-        addresspickerMap.on("addressChanged", function (evt, address) {
-            console.dir(address);
-        });
-        addresspickerMap.on("positionChanged", function (evt, markerPosition) {
-            markerPosition.getAddress(function (address) {
-                if (address) {
-                    $("#addresspicker_map").val(address.formatted_address);
-                }
-            })
-        });
-    }
 
 // #Module - Logo Timeline
-
-
 
 
 
--- application/public/js/maps.js
+++ application/public/js/maps.js
@@ -1,27 +1,160 @@
 var geocoder;
 function initialize() {
   
-  geocoder = new google.maps.Geocoder();
 
   // Create the map.
   var mapOptions = {
     zoom: 2,
-    center: new google.maps.LatLng(38.723888,-35.762143),
-    disableDefaultUI: true,
+    center: new google.maps.LatLng(24.922123, -0.859375),
+ mapTypeControl: true,
+    mapTypeControlOptions: {
+      style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
+    },
+    streetViewControl: false,
+    zoomControl: true,
+    zoomControlOptions: {
+      style: google.maps.ZoomControlStyle.SMALL
+    },
     scrollwheel: false,
-    mapTypeId: google.maps.MapTypeId.TERRAIN,
      styles: [
       {
         "stylers": [
       { "visibility": "simplified" },
       { "weight": 0.5 },
-      { "saturation": 54 },
       { "lightness": 0 },
      { "gamma": .65 },
        
+
+
     ]
       },{
-      }
+        "featureType": "administrative",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "stylers": [
+            {
+                "color": "#5f94ff"
+            },
+            {
+                "lightness": 26
+            },
+            {
+                "gamma": 5.86
+            }
+        ]
+    },
+    {},
+    {
+        "featureType": "road.highway",
+        "stylers": [
+            {
+                "weight": 0.6
+            },
+            {
+                "saturation": -85
+            },
+            {
+                "lightness": 61
+            }
+        ]
+    },
+    {
+        "featureType": "road"
+    },
+    {},
+    {
+        "featureType": "landscape",
+        "stylers": [
+            {
+                "hue": "#0066ff"
+            },
+            {
+                "saturation": 74
+            },
+            {
+                "lightness": 100
+            }
+        ]
+    }
     ]
   };
 
